a7922121de19e22520ff3c876977d733d67a2b05
Roopaksh <72032743+Roopaksh1@users.noreply.github.com>
2023-08-26 23:12:51 +0530

    Add origin in cors

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const app = express();
 app.use(
   cors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'https://instockapp.netlify.app'],
   })
 );
 app.use(express.urlencoded({ extended: false }));
